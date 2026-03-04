9da0f9293686702782c123a5bb65fd2b75b0bc09
Beelzebub <65181309+Beelzebub2@users.noreply.github.com>
2026-03-04 10:24:59 +0000

    Bump app version to 3.9.5

--- UI/app.py
+++ UI/app.py
@@ -51,7 +51,7 @@ version_cache = None
 version_cache_timestamp = 0
 VERSION_CACHE_DURATION = 6 * 60 * 60  # 6 hours in seconds
 
-APP_VERSION = "3.9.4"
+APP_VERSION = "3.9.5"
 UPDATE_MANIFEST_URL = os.environ.get(
     "DND_UPDATE_MANIFEST",
     "https://github.com/Beelzebub2/DnDTools/releases/latest/download/update-manifest.json",
